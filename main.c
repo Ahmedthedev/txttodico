0d4c913650160df775666e0a8b7b9d109b166d0b
ahmedthedev <ahmedhachemi1994@gmail.com>
2016-12-13 23:00:18 +0100

    comment

--- main.c
+++ main.c
@@ -2,59 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 30
-#define LEN 31
-#define SIZE 30
-#define LEN 31
+#define SIZE 30 // Taille la plus grande d'un mots 'Hexakosioihexekontahexaphobie'
+#define LEN 60000 // nombre de mot moyens dans un dico
 
-int main()
-{
-char name[LEN][SIZE]; /* Data records */
-char hold[LEN] ;
-int i,j ; /* indices of array */
-int last ; /* index of last item in array */
+void Capitalize(char string[]);
+// example : "Hello"
+void Capitalize(char string[]){
+    int i;
+    int x = strlen(string);
+     //Efface les caractéres spéciaux
+     for (i=0;i<x;i++){
+         if(string[i] >= 32 && string[i] <= 64  || string[i] >= 122) {
+                string[i] = NULL;
+         }
+    }
 
-FILE *fpPtr; /* fpPtr = sorted_file.txt pointer */
+    for (i=0;i<x;i++){
+         if( i == 0 && string[i] > 96 && string[i] < 123 ) {
+            string[i] -= 32;
 
-/*
-copy file contents to array
-*/
-
-/* fopen opens file; exits program if file cannot be opened */
-if ( ( fpPtr = fopen("readFile.txt", "r" ) ) == NULL ) {
-printf( "File could not be opened" );
-} /* End if */
-
-/* Read records from file */
-else {
-/* While not end of file */
-printf("The names in original order are");
-for(i = 0 ; !feof( fpPtr ) ; i++ ) {
-fscanf( fpPtr, "%s", name[i] );
-printf("%s", name[i]);
-} /* End while */
-last = i - 1 ;
-
-fclose( fpPtr ); /* fclose closes the file */
-
-/*
-sort
-*/
-for (i = last ; i > 0 ; i--)
-for (j = 1 ; j <= i ; j++)
-if (strcmp(name[j],name[j - 1]) < 0) {
-strcpy(hold,name[j]) ;
-strcpy(name[j],name[j - 1]) ;
-strcpy(name[j - 1],hold) ;
+         }else if(i > 0 && string[i] > 64 && string[i] < 96) {
+            string[i] += 32;
+         }
+    }
 }
-/*
-write array to output
-*/
-printf("The names in alphabetical order are");
-for (i = 0 ; i <= last ; i++)
-printf("- %s",name[i]);
 
-} /* End else */
+int main(int argc, char *argv[])
+{
+    char name[LEN][SIZE]; /* Data records */
+    char hold[LEN] ;
+    int i,j ; /* indices of array */
+    int last ; /* index of last item in array */
 
-return 0; /* Indicates that the program terminated successfully */
-} /* End Main */
+    FILE *fpPtr; /* fpPtr = sorted_file.txt pointer */
+    FILE *fpPtrWrite; /* fpPtr = sorted_file.txt pointer */
+
+
+    if ( ( fpPtrWrite = fopen("writeFile.txt", "w" ) ) == NULL ) {
+        printf( "Fichier ne pas pas être ouvert" );
+    }
+
+    if ( ( fpPtr = fopen("readFile.txt", "r" ) ) == NULL ) {
+        printf( "Fichier ne pas pas être ouvert" );
+
+    }else {
+
+        for(i = 0 ; !feof( fpPtr ) ; i++ ) {
+            fscanf( fpPtr, "%s", name[i] );
+            Capitalize(name[i]);
+        }
+        last = i - 1 ;
+        fclose( fpPtr );
+
+        //trie à bull
+        for (i = last ; i > 0 ; i--) {
+            for (j = 1 ; j <= i ; j++) {
+                if(strcmp(name[j],name[j - 1]) == 0) {
+                    strcpy(name[j-1],"");
+                }else if (strcmp(name[j],name[j - 1]) < 0) {
+                    strcpy(hold,name[j]) ;
+                    strcpy(name[j],name[j - 1]) ;
+                    strcpy(name[j - 1],hold) ;
+                }
+            }
+        }
+
+        for (i = 0 ; i <= last ; i++) {
+            if( strcmp(name[i],"") != 0) {
+                if(i == last ) {
+                    fprintf(fpPtrWrite,"%s",name[i]);
+                }else {
+                    fprintf(fpPtrWrite,"%s\n",name[i]);
+                }
+            }
+        }
+    }
+    return 0;
+}
